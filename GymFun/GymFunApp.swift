d64f4053b66cdcb747e51e899f3618cf94bd8c2d
Hans Jonathan Zebua <hans.zebua@icloud.com>
2024-04-24 23:21:22 +0700

    Initial Commit

---
+++ GymFun/GymFunApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymFunApp.swift
+//  GymFun
+//
+//  Created by Hans Zebua on 24/04/24.
+//
+
+import SwiftUI
+
+@main
+struct GymFunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
